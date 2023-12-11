4e23803a240ccf730425c476a91cd3b049f46dde
Mario Canudo <mcanudo22@gmail.com>
2023-12-11 18:04:47 +0000

    daily extract export

--- ICT_Daily_Extract.py
+++ ICT_Daily_Extract.py
@@ -2,9 +2,12 @@ import numpy as np
 import pandas as pd
 from datetime import datetime, timedelta
 
-shift_cutoff = 16
 
-File_Path = r"Z:\Operations - Management\Lisbon Reporting\26. WFM (Reporting)\15. MMT_IW\Attendance 2023-11-23-2023-11-23.xlsx"
+shift_cutoff = 23
+day = 'today'
+
+
+File_Path = r"Z:\Operations - Management\Lisbon Reporting\26. WFM (Reporting)\15. MMT_IW\Attendance 2023-11-27-2023-12-11.xlsx"
 
 
 df = pd.read_excel(File_Path, header=1, usecols=["Primary Group",
@@ -28,9 +31,18 @@ df["Shift_Start"] = pd.to_datetime(df["Shift_Start"])
 df["shift_start_str"] = pd.to_datetime(df.Shift_Start).dt.strftime('%Y-%m-%d %H:%M:%S')
 
 # Filter Shifts
+def shift_filter_function(day):
+    shift_filter = None
+    if day == 'today':
+        shift_filter = str(today + timedelta(hours=shift_cutoff))
+    elif day == 'yesterday':
+        shift_filter = str(yesterday + timedelta(hours=shift_cutoff))
+    return shift_filter
+
 today = datetime.today().date()
 today = pd.to_datetime(today)
-shift_filter = str(today + timedelta(hours=shift_cutoff))
+yesterday = today - timedelta(days=1)
+shift_filter = shift_filter_function(day)
 df = df[df["shift_start_str"] <= shift_filter]
 
 # Remove Timezone from column
@@ -41,4 +53,7 @@ df["status_final"] = df["Event_Name"]
 df["status_final"] = np.where(df["status_final"].isnull(), df["Updated_Status"], df["status_final"])
 df["status_final"] = np.where(df["status_final"].isnull(), df["System_Generated_Status"], df["status_final"])
 
-print(df["Shift_Start"])
+# save backup
+backup_name = 'Backup' + str(shift_filter[:10])
+backup_path = "//lisfs1003/honey_badger$//Operations - Management/WFM/01. IW Report/CO/Daily Report wip/Daily Extract/Daily Extract Backup/%s.csv"%backup_name
+df.to_csv(backup_path, index=False)
